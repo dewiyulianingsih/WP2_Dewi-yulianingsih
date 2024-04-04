d5d7889a3f4c75770b60ff324a569f142dcd201c
dewiyulianingsih <dewiqs981@gmail.com>
2024-04-04 18:11:19 +0700

    pertemuan3

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -19,6 +19,12 @@ class Home extends CI_Controller
         if ($this->session->userdata('email')) {
             $user = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
             $data['user'] = $user['nama'];
+            
+            $this->load->view('templates/templates-user/header', $data);
+            $this->load->view('buku/daftarbuku', $data);
+            $this->load->view('templates/templates-user/footer', $data);
+            $this->load->view('templates/templates-user/modal', $data);
+
         } else {
             $data['user'] = 'Pengunjung';
         }
@@ -26,9 +32,10 @@ class Home extends CI_Controller
         $this->load->view('templates/templates-user/header', $data);
         $this->load->view('buku/daftarbuku', $data);
         $this->load->view('templates/templates-user/footer', $data);
+        $this->load->view('templates/templates-user/modal', $data);
     }
     public function detailBuku()
-{
+    {
     $id = $this->uri->segment(3);
     $buku = $this->ModelBuku->joinKategoriBuku(['buku.id' => $id])->result();
     $data['user'] = "Pengunjung";
@@ -51,5 +58,6 @@ class Home extends CI_Controller
     $this->load->view('templates/templates-user/header', $data);
     $this->load->view('buku/detail-buku', $data);
     $this->load->view('templates/templates-user/footer');
+    $this->load->view('templates/templates-user/modal', $data);
 }
 }
--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -23,10 +23,10 @@
                     <?php if (!empty($this->session->userdata('email'))) { ?>
                         <a class="nav-item nav-link" href="#">Booking Buku</a>
                         <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
-                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
-                    <?php } else { ?>
+                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a><?php } 
+                        else { ?>
                         <a class="nav-item nav-link" data-toggle="modal" data-target="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
-                        <a class="nav-item nav-link" data-toggle="modal" data-target="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
+                        <a class="nav-item nav-link" data-toggle="modal" data-target="#loginModal" href="#loginModal"><i class="fas fw fa-login"></i> Log in</a>
                     <?php } ?>
                     <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px;">Selamat Datang</span>
                 </div>
@@ -34,3 +34,5 @@
         </div>
     </nav>
     <div class="container mt-5">
+
+
